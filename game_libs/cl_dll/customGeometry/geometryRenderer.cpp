8c4177cdd09d41c5f567c7430574ee5a90469270
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2020-03-10 07:38:42 +0000

    Fixed lines not drawing in correct colour

--- game_libs/cl_dll/customGeometry/geometryRenderer.cpp
+++ game_libs/cl_dll/customGeometry/geometryRenderer.cpp
@@ -2,9 +2,16 @@
 #include "cl_dll.h"
 #include "triangleapi.h"
 #include "debug_assert.h"
+#include "hud.h"
+#include "cl_util.h"
 
 namespace CustomGeometry
 {
+	CGeometryRenderer::CGeometryRenderer()
+	{
+		m_ColourSprite = SPR_Load("sprites/white.spr");
+	}
+
 	void CGeometryRenderer::Render(const CGeometryItem& item)
 	{
 		const CUtlVector<Vector>& points = item.GetPoints();
@@ -41,6 +48,17 @@ namespace CustomGeometry
 			return;
 		}
 
+		// This sprite is used as a base texture which we then set the colour of.
+		// For some reason lines don't draw correctly unless a texture is bound...
+		const model_s* colourSprite = gEngfuncs.GetSpritePointer(m_ColourSprite);
+
+		if ( !colourSprite )
+		{
+			return;
+		}
+
+		// UGH. Awful HL const-incorrectness again.
+		gEngfuncs.pTriAPI->SpriteTexture(const_cast<model_s*>(colourSprite), 0);
 		gEngfuncs.pTriAPI->RenderMode(kRenderNormal);
 		gEngfuncs.pTriAPI->Color4ub((colour & 0xFF000000) >> 24,
 									(colour & 0x00FF0000) >> 16,
@@ -57,7 +75,10 @@ namespace CustomGeometry
 			ASSERTSZ(pointIndex0 < static_cast<size_t>(points.Count()), "Index was out of range.");
 			ASSERTSZ(pointIndex1 < static_cast<size_t>(points.Count()), "Index was out of range.");
 
+			gEngfuncs.pTriAPI->TexCoord2f(0.0f, 0.0f);
 			gEngfuncs.pTriAPI->Vertex3fv(points[pointIndex0]);
+
+			gEngfuncs.pTriAPI->TexCoord2f(0.0f, 0.0f);
 			gEngfuncs.pTriAPI->Vertex3fv(points[pointIndex1]);
 		}
 
--- game_libs/cl_dll/customGeometry/geometryRenderer.h
+++ game_libs/cl_dll/customGeometry/geometryRenderer.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include "customGeometry/geometryItem.h"
+#include "cdll_int.h"
 
 namespace CustomGeometry
 {
 	class CGeometryRenderer
 	{
 	public:
+		CGeometryRenderer();
+
 		void Render(const CGeometryItem& item);
 
 	private:
 		void DrawLines(const CGeometryItem& item);
+
+		HSPRITE m_ColourSprite = 0;
 	};
 }
