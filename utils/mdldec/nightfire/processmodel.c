1ced37c2b1a9b3e3f730f3e3922834727fcf7cf8
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2021-04-05 19:00:18 +0100

    Noted that Nightfire MDL decompiler code needs more work

--- utils/mdldec/nightfire/processmodel.c
+++ utils/mdldec/nightfire/processmodel.c
@@ -4,4 +4,8 @@
 void NF_ProcessModel(void)
 {
 	NF_WriteQCScript();
+
+	// TODO: This needs more work.
+	// It's going to be a little bit difficult without access
+	// to the full Nightfire model decompiler code.
 }
